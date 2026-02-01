6d4ae20273e3e79194e9ce1b899ff878ea8848bd
muhammadumergit <muhammadumersmiu@gmail.com>
2026-02-01 01:46:49 -0800

    server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express()
 await connectDB()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
